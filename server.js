c43ff34feb47c4381f806ffaedc6e0609004a238
Aaron <attanner86@gmail.com>
2023-05-09 14:14:22 -0600

    heroku bug fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 const notes = require('./db/db.json')
